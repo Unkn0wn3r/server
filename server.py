688c9af1901d4eb1e74ffcee2ebd85a7e7d5f5a1
Unkn0wn3r <95631862+Unkn0wn3r@users.noreply.github.com>
2022-04-13 00:17:43 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,16 +5,16 @@ hauptkanal_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 hauptkanal_server_socket.bind((socket.gethostname(), 1234))#haupt port
 hauptkanal_server_socket.listen(100)
 
-server_socket.bind((socket.gethostname(), 1944))#1
-server_socket.bind((socket.gethostname(), 1674))#2
-server_socket.bind((socket.gethostname(), 1932))#3
-server_socket.bind((socket.gethostname(), 1987))#4
-server_socket.bind((socket.gethostname(), 1154))#5
-server_socket.bind((socket.gethostname(), 1285))#6
-server_socket.bind((socket.gethostname(), 1239))#7
-server_socket.bind((socket.gethostname(), 9392))#8
-server_socket.bind((socket.gethostname(), 1323))#9
-server_socket.bind((socket.gethostname(), 7777))#10
+server_socket.bind(('192.168.1.127', 1944))#1
+server_socket.bind(('192.168.1.127', 1674))#2
+server_socket.bind(('192.168.1.127', 1932))#3
+server_socket.bind(('192.168.1.127', 1987))#4
+server_socket.bind(('192.168.1.127', 1154))#5
+server_socket.bind(('192.168.1.127', 1285))#6
+server_socket.bind(('192.168.1.127', 1239))#7
+server_socket.bind(('192.168.1.127', 9392))#8
+server_socket.bind(('192.168.1.127', 1323))#9
+server_socket.bind(('192.168.1.127', 7777))#10
 
 server_socket.listen(10)
 
